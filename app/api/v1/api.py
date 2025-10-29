90b1e810baff815d9c52cc221065978622c4107b
leggen-assis <souzaferreira437@gmail.com>
2025-10-29 18:10:00 -0300

    feat: novas rotas adicionadas

--- app/api/v1/api.py
+++ app/api/v1/api.py
@@ -2,7 +2,6 @@ from fastapi import APIRouter
 
 from app.api.v1.endpoints import auxilio_emergencial
 
-
 api_router = APIRouter()
 
-api_router.include_router(auxilio_emergencial.router, prefix = "/auxilio", tags = ["auxilio"])
+api_router.include_router(auxilio_emergencial.router, prefix = "/auxilio", tags = ["Auxílio"])
