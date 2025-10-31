d22924c816371772c579e08a75708532478d5d7a
leggen-assis <souzaferreira437@gmail.com>
2025-10-30 21:23:36 -0300

    refactor: routes splited

--- app/api/v1/api.py
+++ app/api/v1/api.py
@@ -1,7 +1,13 @@
 from fastapi import APIRouter
 
-from app.api.v1.endpoints import auxilio_emergencial
+from app.api.v1.endpoints import (
+    auxilio_emergencial_routes,
+    beneficiario_routes,
+    responsavel_routes
+)
 
-api_router = APIRouter()
+api_router: APIRouter = APIRouter()
 
-api_router.include_router(auxilio_emergencial.router, prefix = "/auxilio", tags = ["Auxílio"])
+api_router.include_router(auxilio_emergencial_routes.router, prefix = "/auxilio", tags = ["Auxílio"])
+api_router.include_router(responsavel_routes.router, prefix = "/responsaveis", tags = ["Responsáveis"])
+api_router.include_router(beneficiario_routes.router, prefix = "/beneficiarios", tags = ["Beneficiários"])
