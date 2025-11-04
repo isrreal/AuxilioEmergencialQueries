57b7852439d355b89fbd1e197faa5adb85411abb
leggen-assis <souzaferreira437@gmail.com>
2025-11-04 11:38:59 -0300

    refactor: routes splitted in 'setup', 'consultas'; login route tag -> 'Autenticação'

--- app/api/v1/api.py
+++ app/api/v1/api.py
@@ -3,11 +3,17 @@ from fastapi.security import OAuth2PasswordRequestForm
 from fastapi.responses import JSONResponse
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from app.api.v1.endpoints import consultas_routes
+from app.api.v1.endpoints import consultas_routes, setup_routes
 from app.core.deps import get_session
 from app.core.auth import criar_token_acesso, autenticar
 
 api_router: APIRouter = APIRouter()
+
+api_router.include_router(
+    setup_routes.router,
+    prefix = "/setup",
+    tags = ["Setup de Índices"]
+)
 
 api_router.include_router(
     consultas_routes.router,
@@ -15,7 +21,10 @@ api_router.include_router(
     tags = ["Consultas"]
 )
 
-@api_router.post("/login", summary = "Autenticar usuário e gerar token JWT")
+@api_router.post(
+    "/login", 
+    summary = "Autenticar usuário e gerar token JWT",
+    tags = ["Autenticação"])
 async def login(
     form_data: OAuth2PasswordRequestForm = Depends(),
     db: AsyncSession = Depends(get_session)
