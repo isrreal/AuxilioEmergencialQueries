e579986ba90a9795c1dcc5ef18abf6a6c4068c35
leggen-assis <souzaferreira437@gmail.com>
2025-11-03 21:02:14 -0300

    refactor: only query routes remains

--- app/api/v1/api.py
+++ app/api/v1/api.py
@@ -3,34 +3,11 @@ from fastapi.security import OAuth2PasswordRequestForm
 from fastapi.responses import JSONResponse
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from app.api.v1.endpoints import (
-    auxilio_routes,
-    beneficiario_routes,
-    responsavel_routes,
-    consultas_routes
-)
+from app.api.v1.endpoints import consultas_routes
 from app.core.deps import get_session
 from app.core.auth import criar_token_acesso, autenticar
 
 api_router: APIRouter = APIRouter()
-
-api_router.include_router(
-    auxilio_routes.router,
-    prefix = "/auxilio",
-    tags = ["Auxílios"]
-)
-
-api_router.include_router(
-    responsavel_routes.router,
-    prefix = "/responsaveis",
-    tags = ["Responsáveis"]
-)
-
-api_router.include_router(
-    beneficiario_routes.router,
-    prefix = "/beneficiarios",
-    tags = ["Beneficiários"]
-)
 
 api_router.include_router(
     consultas_routes.router,
